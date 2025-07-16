186b3dbe63a4be376e0705e571a19bd3d5efa55a
gnavamita <gnavamita@gmail.com>
2025-07-16 14:22:37 +0530

    https://gnavamita.atlassian.net/browse/SCRUM-31

---
+++ src/java_10032025/AdditionOfTwoNumber.java
@@ -0,0 +1,11 @@
+package java_10032025;
+
+public class AdditionOfTwoNumber {
+    public static void main(String args[]) {
+        int a,b;
+        a=2;
+        b=4;
+        int sum = a+b;
+        System.out.println(sum);
+    }
+}
---
+++ src/java_10032025/HelloWorld_1.java
@@ -0,0 +1,10 @@
+package java_10032025;
+
+public class HelloWorld_1 {
+   public static void main(String args[]) {
+      System.out.println("Hello World");
+//hello
+
+
+   }
+}
